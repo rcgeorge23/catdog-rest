e6952499c907a68b93609d70ab04172f9dc0032d
Richard George <rcgeorge23@gmail.com>
2018-04-08 18:25:45 +0100

    increased spring boot max file upload size

--- src/main/java/uk/co/novinet/catdog/rest/Application.java
+++ src/main/java/uk/co/novinet/catdog/rest/Application.java
@@ -2,6 +2,7 @@ package uk.co.novinet.catdog.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
